f113dd09f32ad7db91ca64d15e47eb591b9d78a7
Julien Faure <julien.faure@outlook.com>
2021-08-27 12:21:22 +0200

    Escape algorithm extracted in external class (refactor)

---
+++ EscapeFormComportment.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    public class EscapeFormComportment
+    {
+        private const int XOffset = 20; 
+        private const int YOffset = 30; 
+        private const int XMoveRange = 30; 
+        private const int YMoveRange = 30; 
+        
+        private readonly int _formWidth;
+        private readonly int _formHeight;
+        private readonly int _screenWidth;
+        private readonly int _screenHeight;
+        
+        private int _form1OriginalX;
+        private int _form1OriginalY;
+        private Action<int, int> _updateFormPosition = (i, i1) => { };
+        private int _formX1, _formY1, _formX2, _formY2;
+
+        public EscapeFormComportment(int formWidth, int formHeight, int screenWidth, int screenHeight)
+        {
+            _formWidth = formWidth;
+            _formHeight = formHeight;
+            _screenWidth = screenWidth;
+            _screenHeight = screenHeight;
+        }
+        
+        public void SetUpdateFormPositionEvent(Action<int,int> action){
+            _updateFormPosition = action;
+        }
+
+        public void UpdateMousePosition(int cursorX, int cursorY, int formX, int formY)
+        {
+            CalculatePositions(formX, formY);
+
+            bool cursorIsInXRange = cursorX > _formX1 && cursorX < _formX2;
+            bool cursorIsInYRange = cursorY > _formY1 && cursorY < _formY2;
+
+            if (cursorIsInXRange && cursorIsInYRange)
+            {
+                MakeTheFormEscapeFromTheCursor(cursorX, cursorY);
+                HandleScreenBoundsCollision();
+            }
+        }
+
+        private void CalculatePositions(int formX, int formY)
+        {
+            _form1OriginalX = formX;
+            _form1OriginalY = formY;
+
+            _formX1 = _form1OriginalX - XOffset;
+            _formY1 = _form1OriginalY - YOffset;
+            _formX2 = _formX1 + _formWidth + XOffset;
+            _formY2 = _formY1 + _formHeight + YOffset;
+        }
+
+        private void MakeTheFormEscapeFromTheCursor(int cursorX, int cursorY)
+        {
+            bool ifCursorIsInFirstHalfOfXRange = cursorX > _formX1 && cursorX < (_formX1 + (_formWidth / 2));
+            bool ifCursorIsInFirstHalfOfYRange = cursorY > _formY1 && cursorY < (_formY1 + (_formHeight / 2));
+
+            if (ifCursorIsInFirstHalfOfXRange && ifCursorIsInFirstHalfOfYRange)
+                _updateFormPosition(_form1OriginalX + XMoveRange, _form1OriginalY + YMoveRange);
+            else if (ifCursorIsInFirstHalfOfXRange) //&& !ifCursorIsInFirstHalfOfYRange
+                _updateFormPosition(_form1OriginalX + XMoveRange, _form1OriginalY - YMoveRange);
+            else if (ifCursorIsInFirstHalfOfYRange) //&& !ifCursorIsInFirstHalfOfXRange
+                _updateFormPosition(_form1OriginalX - XMoveRange, _form1OriginalY + YMoveRange);
+            else
+                _updateFormPosition(_form1OriginalX - XMoveRange, _form1OriginalY - YMoveRange);
+        }
+
+        private void HandleScreenBoundsCollision()
+        {
+            if ((_form1OriginalX + _formWidth) >= _screenWidth)
+                _updateFormPosition(0, _form1OriginalY);
+            else if (_form1OriginalX < 0)
+                _updateFormPosition(_screenWidth - _formWidth, _form1OriginalY);
+
+            if ((_form1OriginalY + _formHeight) >= _screenHeight)
+                _updateFormPosition(_form1OriginalX, 0);
+            else if (_form1OriginalY < 0)
+                _updateFormPosition(_form1OriginalX, _screenHeight - _formHeight);
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -5,11 +5,7 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form,CursorPositionUser
     {
-
-        private const int XOffset = 20; 
-        private const int YOffset = 30; 
-        private const int XMoveRange = 30; 
-        private const int YMoveRange = 30; 
+        private readonly EscapeFormComportment _escapeFormComportment;
         
         public Form1()
         {
@@ -19,45 +15,20 @@ namespace WindowsFormsApp1
                 MessageBox.Show("Bravo tu as réussi :(", "Pfff...", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 this.Hide();
             };
-        }
 
+            _escapeFormComportment = new EscapeFormComportment(
+                Width, 
+                Height,
+                Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height);
+            
+            _escapeFormComportment.SetUpdateFormPositionEvent((x, y) => this.DesktopLocation = new Point(x,y));
+        }
 
         public void SetCursorPosition(Point position)
         {
-            int cursorX = position.X;
-            int cursorY = position.Y;
-            int formX1 = this.DesktopLocation.X - XOffset;
-            int formY1 = this.DesktopLocation.Y - YOffset;
-            int formX2 = formX1 + this.Width + XOffset;
-            int formY2 = formY1 + this.Height + YOffset;
-
-            bool cursorIsInXRange = cursorX > formX1 && cursorX < formX2;
-            bool cursorIsInYRange = cursorY > formY1 && cursorY < formY2;
-            
-            if (cursorIsInXRange && cursorIsInYRange)
-            {
-                bool ifCursorIsInFirstHalfOfXRange = cursorX > formX1 && cursorX < (formX1 + (this.Width / 2));
-                bool ifCursorIsInFirstHalfOfYRange = cursorY > formY1 && cursorY < (formY1 + (this.Height / 2));
-
-                if (ifCursorIsInFirstHalfOfXRange && ifCursorIsInFirstHalfOfYRange)
-                    this.DesktopLocation = new Point(this.DesktopLocation.X + XMoveRange, this.DesktopLocation.Y + YMoveRange);
-                else if (ifCursorIsInFirstHalfOfXRange) //&& !ifCursorIsInFirstHalfOfYRange
-                    this.DesktopLocation = new Point(this.DesktopLocation.X + XMoveRange, this.DesktopLocation.Y - YMoveRange);
-                else if (ifCursorIsInFirstHalfOfYRange) //&& !ifCursorIsInFirstHalfOfXRange
-                    this.DesktopLocation = new Point(this.DesktopLocation.X - XMoveRange, this.DesktopLocation.Y + YMoveRange);
-                else
-                    this.DesktopLocation = new Point(this.DesktopLocation.X - XMoveRange, this.DesktopLocation.Y - YMoveRange);
-
-                if ((this.DesktopLocation.X + this.Width) >= Screen.PrimaryScreen.Bounds.Width)
-                    this.DesktopLocation = new Point(0, this.DesktopLocation.Y);
-                else if (this.DesktopLocation.X < 0)
-                    this.DesktopLocation = new Point(Screen.PrimaryScreen.Bounds.Width - this.Width, this.DesktopLocation.Y);
-
-                if((this.DesktopLocation.Y + this.Height) >= Screen.PrimaryScreen.Bounds.Height)
-                    this.DesktopLocation = new Point(this.DesktopLocation.X, 0);
-                else if (this.DesktopLocation.Y < 0)
-                    this.DesktopLocation = new Point(this.DesktopLocation.X, Screen.PrimaryScreen.Bounds.Height - this.Height);
-            }
+            _escapeFormComportment.UpdateMousePosition(position.X,position.Y, 
+                DesktopLocation.X, DesktopLocation.Y);
         }
     }
 }
